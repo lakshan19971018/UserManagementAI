ac08a848d85874611460e3ab74e4353398319da9
lakshan <lahiru.lakshan@spemai.com>
2025-02-21 13:43:15 +0530

    chnage admine

--- backend/app/db/models/admin.py
+++ backend/app/db/models/admin.py
@@ -8,4 +8,4 @@ class Admin(Base):
     id = Column(Integer,primary_key=True,index=True)
     email  = Column(String(255), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
-    created_at = Column(DateTime, nullable=False,server_default=func.now())
+    #created_at = Column(DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
